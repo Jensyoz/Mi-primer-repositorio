7d887d3fa5f6f4fd24b5815454395fc7d504d63e
Jensyoz <85911988+Jensyoz@users.noreply.github.com>
2021-07-08 19:33:48 -0600

    Update Practica 3.R
    actualizacion

--- Practica 3.R
+++ Practica 3.R
@@ -3,4 +3,21 @@
 # Grafico Correlaciòn
 View(Potabilidad_del_agua)
 
-Plot(select(Potabilidad_del_agua,solids,Organic))
+plot(select(Potabilidad_del_agua,Solids,Organic_carbon,Conductivity))
+
+# Grafico Barras
+
+Potabilidad_del_aguafiltro = filter(Potabilidad_del_agua, Sulfate <250 & Potability == 1)
+View(Potabilidad_del_aguafiltro)
+
+
+plot(as.factor(Potabilidad_del_aguafiltro$Potability ))
+plot(as.factor(Potabilidad_del_aguafiltro$Potability ),Potabilidad_del_aguafiltro$Sulfate)
+
+
+
+#· time based
+
+mis_fechas <- seq(as.Date("2014/1/1"), by = "month", length = 50 ) 
+
+plot(mis_fechas,rnorm(50), type = "l")
